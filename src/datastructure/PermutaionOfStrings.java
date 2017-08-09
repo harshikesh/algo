cb37b9d8fea5ddc502cfab6b6038c1759a9a9b32
Harshikesh Kumar <harshikeshkumar@gmail.com>
2017-08-09 15:04:55 +0530

    change permString

--- src/datastructure/PermutaionOfStrings.java
+++ src/datastructure/PermutaionOfStrings.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by harshikesh.kumar on 06/03/17.
  */
@@ -22,5 +25,24 @@ public class PermutaionOfStrings {
         for (int i = 0; i < s.length(); i++) {
             generatePerm(pre+s.charAt(i),s.substring(0,i)+s.substring(i+1));
         }
+    }
+
+    public static <T> List<ArrayList<T>> checkPerm(List<T> listData) {
+        List<ArrayList<T>> result = new ArrayList<ArrayList<T>>();
+        result.add(new ArrayList<T>());
+        for (int i = 0; i < listData.size(); i++) {
+            ArrayList<ArrayList<T>> current = new ArrayList<ArrayList<T>>();
+
+            for (ArrayList<T> l : result) {
+                for (int j = 0; j < l.size() + 1; j++) {
+                    l.add(j, listData.get(i));
+                    ArrayList<T> temp = new ArrayList<T>(l);
+                    current.add(temp);
+                    l.remove(j);
+                }
+            }
+            result = new ArrayList<ArrayList<T>>(current);
+        }
+        return result;
     }
 }
